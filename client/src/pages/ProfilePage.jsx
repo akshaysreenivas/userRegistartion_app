aa4c6c3b34b58c391af6c0d11b1a82393ad66c42
akshay <akshaypvkeerakkal@gmail.com>
2023-05-17 16:51:05 +0530

    last commit

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Profile from '../components/profile/Profile';
+import { useNavigate } from 'react-router-dom';
 
 function ProfilePage() {
- 
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("userAuthToken");
+    if (!token || token === "undefined") return navigate("/register");
+  }, [navigate]);
 
-  return (
-    <div>ProfilePage</div>
-  )
+  return <Profile />;
 }
 
 export default ProfilePage
